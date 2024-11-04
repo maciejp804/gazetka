8924a49375950022a24e7ee577dd70cba1f6e65d
maciejp804 <maciej.puchalski@onet.eu>
2024-11-04 10:43:48 +0100

    brak podświetlenia dla select i input

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,16 +1,16 @@
 @props(['placeholder' => 'Wyszukaj np. masło, Lidl' , 'border' => false])
 
 @php
-    $classes = 'w-full bg-white-50 rounded-3xl text-sm placeholder-gray-400';
+    $classes = 'w-full bg-white-50 rounded-3xl text-sm placeholder-gray-400 focus:outline-none focus:ring-0';
         if ($border)
-            $classes .= ' border border-gray-200';
+            $classes .= ' border border-gray-200 focus:border-gray-200';
         else
             $classes .= ' border-none pl-11'
 @endphp
 
 <div {{$attributes->merge(['class' => 'w-full h-12'])}}>
         <div class="flex w-full relative h-full">
-            <input type="text" name="search" placeholder="{{$placeholder}}" class="{{$classes}}">
+            <input type="text" name="search" placeholder="{{$placeholder}}" class="{{$classes}}" autocomplete="off">
             {{ $slot }}
         </div>
 </div>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->merge(['class' => 'w-full h-12']) }}>
     <div class="flex w-full relative h-full">
-        <select type="text" name="search" class="w-full border border-gray-200 text-gray-400 bg-white-50 rounded-3xl text-sm/[24px] focus:border focus:!border-transparent">
+        <select type="text" name="search" class="w-full border border-gray-200 text-gray-400 bg-white-50 rounded-3xl text-sm/[24px] focus:ring-0 focus:border-gray-200">
             <option selected>Wszystkie</option>
             <option>Dom</option>
             <option>Art. Spożywcze</option>
