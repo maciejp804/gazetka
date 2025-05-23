1c96504f551cf4ea6c4a3bd2454a25a160d02b1a
maciejp804 <maciej.puchalski@onet.eu>
2025-05-23 11:23:21 +0200

    Update formularza dodawania i edycji gazetek, daoaadnie reklam winietowych, update admanager.blade.php i admanager.php v2

--- resources/views/components/leaflet-product-results.blade.php
+++ resources/views/components/leaflet-product-results.blade.php
@@ -31,17 +31,45 @@
 
                     @if($adsStatus === true)
                         @switch($loop->iteration)
-                            @case(5)
-                                <x-ad-1 class="hidden lg:grid lg:col-span-5 lg:my-5"/>
+                            @case(10)
+                                {{-- Reklama pozioma - 1--}}
+                                <div class="hidden lg:grid lg:col-span-5 lg:my-5 lg:min-h-75 mx-auto">
+                                    <div class="relative before:content-['Reklama'] before:absolute before:-top-5 before:left-0 before:text-1xs before:uppercase before:tracking-wide before:text-gray-500"></div>
+                                    <x-admanager
+                                        slot-name="subdomain_middle_desktop_2"
+                                        :overrides="['page' => 'subdomain.index']"
+                                    />
+                                </div>
                                 @break
-
                             @case(3)
-                                <x-ad-1 class="hidden sm:grid sm:col-span-3 sm:my-5 lg:hidden"/>
+                                {{-- Reklama pozioma - 1--}}
+                                <div class="hidden sm:grid sm:col-span-3 sm:my-5 sm:min-h-75 mx-auto lg:hidden">
+                                    <div class="relative before:content-['Reklama'] before:absolute before:-top-5 before:left-0 before:text-1xs before:uppercase before:tracking-wide before:text-gray-500"></div>
+                                    <x-admanager
+                                        slot-name="subdomain_middle_desktop_1"
+                                        :overrides="['page' => 'subdomain.index']"
+                                    />
+                                </div>
                                 @break
                             @case(2)
+                                {{-- Reklama pozioma mobile- 1--}}
+                                <div class="col-span-2 my-5 min-h-70 mx-auto sm:hidden">
+                                    <div class="relative before:content-['Reklama'] before:absolute before:-top-5 before:left-0 before:text-1xs before:uppercase before:tracking-wide before:text-gray-500"></div>
+                                    <x-admanager
+                                        slot-name="subdomain_middle_mobile_1"
+                                        :overrides="['page' => 'subdomain.index']"
+                                    />
+                                </div>
+                                @break
                             @case(6)
-                            @case(12)
-                                <x-ad-1 class="col-span-2 my-5 sm:hidden"/>
+                                {{-- Reklama pozioma mobile- 2--}}
+                                <div class="col-span-2 my-5 min-h-70 mx-auto sm:hidden">
+                                    <div class="relative before:content-['Reklama'] before:absolute before:-top-5 before:left-0 before:text-1xs before:uppercase before:tracking-wide before:text-gray-500"></div>
+                                    <x-admanager
+                                        slot-name="subdomain_middle_mobile_2"
+                                        :overrides="['page' => 'subdomain.index']"
+                                    />
+                                </div>
                                 @break
                         @endswitch
                     @endif
