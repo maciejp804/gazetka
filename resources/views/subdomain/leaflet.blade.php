16ce12ce10191a135faf768aab2336c06b9da065
maciejp804 <maciej.puchalski@onet.eu>
2025-05-24 07:00:06 +0200

    poprawka do reklam -v2

--- resources/views/subdomain/leaflet.blade.php
+++ resources/views/subdomain/leaflet.blade.php
@@ -78,7 +78,7 @@
                     :ads="$ads"
                 />
             </x-section>
-            <div class="hidden lg:flex lg:my-5 lg:min-h-75 mx-auto">
+            <div class="hidden lg:flex lg:justify-center lg:my-5 lg:min-h-75 mx-auto">
                 <div class="relative before:content-['Reklama'] before:absolute before:-top-5 before:left-0 before:text-1xs before:uppercase before:tracking-wide before:text-gray-500"></div>
                 <x-admanager
                     slot-name="subdomain_middle_desktop_3"
