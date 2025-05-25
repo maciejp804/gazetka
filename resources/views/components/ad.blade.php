7d747d3630169b0dbaa802f956da947bb72818d4
maciejp804 <maciej.puchalski@onet.eu>
2025-05-25 22:26:30 +0200

    poprawka do reklam -v2

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,11 +1,15 @@
-<div class="hidden md:flex">
-    <ins class="adsbygoogle"
-         style="display:inline-block;width:750px;height:300px"
-         data-ad-client="ca-pub-0504184268109752"
-         data-ad-slot="9092204614"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+<div class="hidden md:flex" id="">
+    <div class="max-w-7xl">
+        <ins class="adsbygoogle" id="ad1"
+             style="display:inline-block;width:750px;height:300px"
+             data-ad-client="ca-pub-0504184268109752"
+             data-ad-slot="9092204614"></ins>
+        <script>
+            if (document.getElementById('ad1')?.offsetWidth > 0) {
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            }
+        </script>
+    </div>
 </div>
 
 <div class="flex md:hidden">
