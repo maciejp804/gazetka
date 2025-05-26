8eeb968187857088b71391bf59cd375b5e5c845f
maciejp804 <maciej.puchalski@onet.eu>
2025-05-26 23:41:53 +0200

    poprawka do robots

--- routes/web.php
+++ routes/web.php
@@ -29,23 +29,6 @@ use App\Http\Controllers\Admin\HotSpotController as AdminHotSpotController;
 use App\Http\Controllers\Admin\BlogController as AdminBlogController;
 use App\Http\Controllers\Admin\DescriptionController as AdminDescriptionController;
 
-Route::middleware([])->get('/robots.txt', function () {
-    $host = request()->getHost();
-    $mainDomain = 'gazetkapromocyjna.com.pl';
-
-    $isMain = $host === $mainDomain || $host === 'www.' . $mainDomain;
-
-    $sitemapUrl = $isMain
-        ? secure_url('/sitemaps/sitemap-main.xml')
-        : secure_url('/sitemaps/sitemap-' . explode('.', $host)[0] . '.xml');
-
-    $robots = app()->environment('production')
-        ? "User-agent: *\nDisallow:\n\nSitemap: {$sitemapUrl}"
-        : "User-agent: *\nDisallow: /\n\nSitemap: {$sitemapUrl}";
-
-    return response($robots, 200)
-        ->header('Content-Type', 'text/plain');
-});
 
 $mainDomain = config('app.main_domain');
 
