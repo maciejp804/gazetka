268389ec25e79528c771b2af23d2f08690d0f785
maciejp804 <maciej.puchalski@onet.eu>
2025-05-31 07:15:43 +0200

    robots.txt w web.php

--- routes/web.php
+++ routes/web.php
@@ -229,15 +229,17 @@ Route::domain('{subdomain}.'.$mainDomain)->group(function () {
         ->where('address', '[a-z0-9_-]+') // tylko alfanumeryczne i myślniki
         ->name('subdomain.shop_address');
 
-//    Route::get('/robots.txt', function () {
-//        $content = file_get_contents(public_path('robots.real.txt'));
-//        Log::info('robots.txt accessed', [
-//            'ip' => request()->ip(),
-//            'user_agent' => request()->userAgent(),
-//            'content' => $content,
-//        ]);
-//        return response($content, 200)->header('Content-Type', 'text/plain');
-//    });
+    Route::get('/robots.txt', function ($subdomain) {
+        $domain = "$subdomain." . config('app.main_domain');
+        $sitemap = "https://$domain/sitemaps/sitemap-$subdomain.xml";
+        Log::info('robots.txt accessed', [
+            'ip' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+            'subdomain' => $sitemap,
+        ]);
+        return response("User-agent: *\nDisallow:\n\nSitemap: $sitemap", 200)
+            ->header('Content-Type', 'text/plain');
+    });
 
     Route::get('/{community}', [MainController::class, 'subdomainIndexGps'])
         ->where('community', '[a-z0-9_-]+') // tylko alfanumeryczne i myślniki
@@ -327,16 +329,15 @@ Route::domain($mainDomain)->group(function () {
 
     require __DIR__.'/auth.php';
 
-
-//    Route::get('/robots.txt', function () {
-//        $content = file_get_contents(public_path('robots.real.txt'));
-//        Log::info('robots.txt accessed', [
-//            'ip' => request()->ip(),
-//            'user_agent' => request()->userAgent(),
-//            'content' => $content,
-//        ]);
-//        return response($content, 200)->header('Content-Type', 'text/plain');
-//    });
+    Route::get('/robots.txt', function () {
+        $content = file_get_contents(public_path('robots.real.txt'));
+        Log::info('robots.txt accessed', [
+            'ip' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+            'content' => $content,
+        ]);
+        return response($content, 200)->header('Content-Type', 'text/plain');
+    });
 
 
    //Main
